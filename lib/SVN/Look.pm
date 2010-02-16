280b247c40ecbcab7d7f39206699ccb3e5c3bd4e
gnustavo <gnustavo@7a6e54f8-c007-11dd-824f-6f77523b18d3>
2010-02-16 20:14:59 +0000

    bumpping to version 0.16
    git-svn-id: https://svn-look.googlecode.com/svn/trunk@23 7a6e54f8-c007-11dd-824f-6f77523b18d3

--- lib/SVN/Look.pm
+++ lib/SVN/Look.pm
@@ -11,11 +11,11 @@ SVN::Look - A caching wrapper aroung the svnlook command.
 
 =head1 VERSION
 
-Version 0.15
+Version 0.16
 
 =cut
 
-our $VERSION = '0.15';
+our $VERSION = '0.16';
 
 =head1 SYNOPSIS
 
